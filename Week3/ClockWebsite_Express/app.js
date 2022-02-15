44371b4366ba59ef6bbd091d9be14b7cb2861013
OliverChrestensen <oliver.chrestensen@hotmail.com>
2022-02-15 16:36:45 +0100

    heroku

---
+++ Week3/ClockWebsite_Express/app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const { send } = require("process");
+const app = express();
+
+app.get("/",(req,res) => {
+    res.send(`
+        <h1> Welcome to mY website</h1>`);
+});
+
+app.get("/clock",(req,res)=>{
+res.sendFile(__dirname + "/public/.html")
+
+});
+
+app.listen(process.env.PORT);
