9a69ea2ef2454eb96739b4cb4cc789cd71a35b89
OliverChrestensen <oliver.chrestensen@hotmail.com>
2022-02-15 13:04:17 +0100

    loops

---
+++ Week3/loops.js
@@ -0,0 +1,31 @@
+//Smartphones
+
+//why dont we like for loops in JS
+//Overly verbose
+//Error prone
+//not functional
+
+const smartPhones = [
+    { brand: "Samsung", price: 4500},
+    { brand: "iPhone", price: 7200 },
+    { brand: "Sony Experia", price: 3800}
+    
+];
+
+//task create a phone sale by subtracting 500 from each phone
+
+const discointedPhones = smartPhones.map(smartPhone =>{
+    smartPhone.price -= 500;
+    return smartPhone;
+
+});
+
+//task i am a poor boy, remove all smartphones that costs over 4000
+
+const PhonesUnder4000 = smartPhones.filter(smartPhone => smartPhone.price <= 4000);
+
+    
+
+
+
+
