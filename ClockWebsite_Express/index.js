f1154ec307f1a0b79083de9cac22e5202b32354a
Oliverchrestensen <oliverchrestensen@hotmail.com>
2022-02-21 12:39:47 +0100

    virker det her nu?

--- ClockWebsite_Express/index.js
+++ ClockWebsite_Express/index.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const { send } = require("process");
 const app = express();
 
 app.get("/",(req,res) => {
@@ -7,9 +6,5 @@ app.get("/",(req,res) => {
         <h1> Welcome to mY website</h1>`);
 });
 
-app.get("/clock",(req,res)=>{
-res.sendFile(__dirname + "/public/.html")
-
-});
 
 app.listen(process.env.PORT || 5000);
