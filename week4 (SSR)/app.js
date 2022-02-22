51d5cf4dba3618e171a9513c4ef303066e940212
Oliverchrestensen <oliverchrestensen@hotmail.com>
2022-02-22 20:33:58 +0100

    week 4 ssr

---
+++ week4 (SSR)/app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+
+app.use(express.static("public"));
+
+const fs = require("fs");
+
+const nav = fs.readFileSync("./public/components/nav/nav.html").toString();
+const footer = fs.readFileSync("./public/components/footer/footer.html").toString();
+
+const frontpage = fs.readFileSync("./public/pages/frontpage/frontpage.html").toString();
+const themepark = fs.readFileSync("./public/pages/themepark/themepark.html").toString();
+
+
+const frontpagePage = nav.replace("%%TITLE_PLACEHOLDER%%", "The Theme Park") + frontpage + footer;
+const themeparkPage = nav.replace("%%CSS_PLACEHOLDER%%", "/components/nav/global.css") + themepark + footer;
+
+
+app.get("/", (req, res) => {
+    res.send(frontpagePage);
+});
+
+app.get("/themepark", (req, res) => {
+    res.send(themeparkPage);
+});
+
+const PORT = 8080;
+app.listen(PORT, () => {
+    console.log("Server started on port:", PORT);
+});
