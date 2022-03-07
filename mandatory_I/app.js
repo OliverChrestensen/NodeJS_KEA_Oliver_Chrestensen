e0fedba197df7726704c44c458292086b02c46d5
Oliverchrestensen <oliverchrestensen@hotmail.com>
2022-03-07 12:28:17 +0100

    mandatory done

--- mandatory_I/app.js
+++ mandatory_I/app.js
@@ -1,3 +1,4 @@
+
 const express = require("express");
 const app = express();
 
@@ -9,13 +10,33 @@ const frontpage = fs.readFileSync("./public/pages/frontpage/frontpage.html").toS
 
 const nav = fs.readFileSync("./public/components/nav/nav.html").toString();
 
+const variables = fs.readFileSync("./public/pages/variables/variables.html").toString();
+
+const RESTAPI = fs.readFileSync("./public/pages/RESTAPI/RESTAPI.html").toString();
+
+const expresshtml = fs.readFileSync("./public/pages/express/express.html").toString();
+
+const nodemon = fs.readFileSync("./public/pages/nodemon/nodemon.html").toString();
 
 app.get("/", (req, res) => {
     res.send(nav+frontpage);
 });
 
+app.get("/variables", (req, res) => {
+    res.send(nav+variables);
+});
 
+app.get("/RESTAPI", (req, res) => {
+    res.send(nav+RESTAPI);
+});
 
+app.get("/express", (req, res) => {
+    res.send(nav+expresshtml);
+});
+
+app.get("/nodemon", (req, res) => {
+    res.send(nav+nodemon);
+});
 
 
 const PORT = 8080;
