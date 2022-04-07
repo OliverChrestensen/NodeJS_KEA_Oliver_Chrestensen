c7efbaf5f3b67aa141ad76bc99c8ce9dcb4d3721
Oliverchrestensen <oliverchrestensen@hotmail.com>
2022-04-07 13:01:17 +0200

    mandatory II

---
+++ mandatory_II/server/app.js
@@ -0,0 +1,19 @@
+import express from "express";
+const app = express();
+
+/*import cors from "cors";
+app.use(cors());*/
+
+
+import path from "path";
+app.use(express.static(path.resolve("../client/public")));
+
+import router from "./routers/playersRouter.js";
+app.use(router);
+
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log("Starting server on port", PORT)
+})
